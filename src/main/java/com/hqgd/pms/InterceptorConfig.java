ee6f4c5cc8afc4af3751e66488e27c52bf012db9
laofuziABC <panshuaiya@126.com>
2018-11-28 11:24:16 +0800

    修改拦截器配置

---
+++ src/main/java/com/hqgd/pms/InterceptorConfig.java
@@ -0,0 +1,43 @@
+package com.hqgd.pms;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 自定义拦截器拦截规则
+ * 需要注意的是SpringBoot 2.0以前，是通过继承WebMvcConfigurerAdapter类
+ * 但在SpringBoot 2.0之后，需要通过实现WebMvcConfigurer接口。
+ * @author laofuzi 2018.11.28
+ */
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+	
+	/**
+	 * 先注册组件
+	 * 将自定义的拦截器注入到配置中
+	 * @return
+	 */
+	@Bean
+	MyInterceptor interceptor() {
+		return new MyInterceptor();
+	}
+	
+	/**
+	 * 配置拦截器的拦截规则
+	 * addPathPatterns ——添加拦截规则
+	 * excludePathPatterns ——排除拦截
+	 * 需要注意的是，在配置拦截规则时，
+	 * 		“/*”		——表示的是当前路径下的一级url地址
+	 * 		“/**”	——表示的是当前目录下的所有层级url地址(甚至静态资源)
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor()).addPathPatterns("/*")
+            .excludePathPatterns("/hqgd")							//登录页
+			.excludePathPatterns("classpath:/static/**");	//静态资源
+    }
+
+}
--- src/main/resources/config/MyConfig.java
+++ src/main/resources/config/MyConfig.java
@@ -5,26 +5,16 @@
  */
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootConfiguration
 public class MyConfig extends WebMvcConfigurerAdapter {
-	private MyInterceptor myInterceptor;
+	
 	//设置静态资源加载路径
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 	
-	//配置拦截器的拦截规则
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-        //addPathPatterns 用于添加拦截规则
-        //excludePathPatterns 用于排除拦截
-        registry.addInterceptor(myInterceptor).addPathPatterns("/**")
-            .excludePathPatterns("/hqgd")					//登录页
-            .excludePathPatterns("/hqgd/login");		//用户登录
-        super.addInterceptors(registry);
-    }
 }
