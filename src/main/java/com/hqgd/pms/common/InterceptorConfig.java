88d08097de9097e916a6884ef55908bfaae5b3f6
laofuziABC <panshuaiya@126.com>
2018-12-12 11:43:00 +0800

    首页嵌入地图。

--- src/main/java/com/hqgd/pms/common/InterceptorConfig.java
+++ src/main/java/com/hqgd/pms/common/InterceptorConfig.java
@@ -37,6 +37,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(interceptor()).addPathPatterns("/*")
             .excludePathPatterns("/hqgd")							//登录页
+            .excludePathPatterns("/hqgd/login")							//登录页
             .excludePathPatterns("/user/*")
             .excludePathPatterns("/equipment/*")
 			.excludePathPatterns("classpath:/static/**");	//静态资源
