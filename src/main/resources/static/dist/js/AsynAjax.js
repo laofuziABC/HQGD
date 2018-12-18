04965c9d5aeca46baf4f303d093d07b20f3b197b
laofuziABC <panshuaiya@126.com>
2018-12-18 18:38:54 +0800

    修改“实时监控统计图”；微调页面样式。

--- src/main/resources/static/dist/js/AsynAjax.js
+++ src/main/resources/static/dist/js/AsynAjax.js
@@ -21,93 +21,16 @@ function POSTAjaxForAlert(url,param,message){
 		});
 	}
 }
-//异步获取用户列表
-function POSTAjaxForUserList(url,param,mark){
+//异步获取设备/用户列表
+function POSTAjaxForAllResultList(url,param,mark){
+	var allResultList={};
 	if(url!=null && url!=""){
 		$.ajax({
-			url: url,
-			type: "post",
-			data: param,
-			dataType: "json",
-			success: function(result){
-				var userlist = "";
-				//设置分页信息——当前为假分页
-				var totalSize = result.length;
-				var pageSize = 5;
-				var totalNum = Math.ceil(totalSize/pageSize);
-				var startIndex = (pageNum-1)*pageSize;
-				var endIndex = (pageNum*pageSize>totalSize)?totalSize:pageNum*pageSize;
-				for(let i=startIndex; i<endIndex; i++){
-					userlist += "<tr class=\"\">";
-					userlist += "<td>"+(i+1)+"</td>";
-					userlist += "<td>"+result[i].userName+"</td>";
-					userlist += "<td>"+result[i].comments+"</td>";
-					userlist += "<td>"+parent.formatDateToString(new Date(result[i].createTime))+"</td>";
-					userlist += "<td>"+parent.formatDateToString(new Date(result[i].updateTime))+"</td>";
-					userlist += "<td>"+result[i].updater+"</td>";
-					if(result[i].isdel == "N"){userlist += "<td>启用</td>"; }
-					else{userlist += "<td>已删除</td>";}
-					userlist += "<td class=\"center\">";
-					userlist += "<a class=\"edit\" href=\"#\" onclick=\"getUserEdit('"+result[i].id+"');\">编辑</a>&nbsp;";
-					userlist += "<a class=\"delete\" href=\"#\" onclick=\"getUserDelete('"+result[i].id+"');\">删除</a>&nbsp;";
-					userlist += "<a class=\"resetpsw\" href=\"#\" onclick=\"initUserPassword('"+result[i].id+"');\">密码初始化</a>";
-					userlist += "</td>";
-					userlist += "</tr>";
-				}
-				$("#userList").html(userlist);
-				if(mark){
-					//加载页码
-					$("#user-list-pager").html(loadPages(pageSize,totalSize,pageNum));
-					$("#user-list-pager").setActivePage(2);
-				}
-			}
+			url: url, type: "post", data: param, dataType: "json",
+			success: function(result){allResultList=result; }
 		});
 	}
-}
-//异步获取设备列表
-function POSTAjaxForEquiList(url,param,mark){
-	if(url!=null && url!=""){
-		$.ajax({
-			url: url,
-			type: "post",
-			data: param,
-			dataType: "json",
-			success: function(result){
-				var data = result.data;
-				//设置分页信息——当前为假分页
-				var equipList = "";
-				var totalSize = data.length;
-				var pageSize = 5;
-				var totalNum = Math.ceil(totalSize/pageSize);
-				var startIndex = (pageNum-1)*pageSize;
-				var endIndex = (pageNum*pageSize>totalSize)?totalSize:pageNum*pageSize;
-				for(let i=startIndex; i<endIndex; i++){
-					equipList+="<tr>";
-					equipList+="<td>"+(i+1)+"</td>";
-					equipList+="<td>"+data[i].equipmentName+"</td>";
-					equipList+="<td>"+data[i].address+"</td>";
-					equipList+="<td>"+data[i].adcode+"</td>";
-					equipList+="<td>"+data[i].lngLat+"</td>";
-					equipList+="<td>"+data[i].frameStru+"</td>";
-					equipList+="<td>"+data[i].userName+"</td>";
-					equipList+="<td>"+parent.formatDateToString(new Date(data[i].createTime))+"</td>";
-					equipList+="<td>"+parent.formatDateToString(new Date(data[i].updateTime))+"</td>";
-					equipList+="<td>"+data[i].updater+"</td>";
-					equipList+="<td class=\"center\">";
-					equipList+="<a class=\"edit\" href=\"#\" onclick=\"getEquiEdit('"+data[i].equipmentId+"');\">编辑</a>&nbsp;";
-					equipList+="<a class=\"delete\" href=\"#\"onclick=\"getEquiDelete('"+data[i].equipmentId+"');\">删除</a>&nbsp;";
-					equipList+="</td>";
-					equipList+="</tr>";
-				}
-				$("#equipmentList").html(equipList);
-				if(mark){
-					//加载页码
-					$("#equi-list-pager").html(loadPages(pageSize,totalSize,pageNum));
-					$("#equi-list-pager").setActivePage(3);
-				}
-			}
-		});
-	}
+	return allResultList;
 }
 //封装数据监测页面的Ajax异步请求
 var equiId;		//设备主键
@@ -124,21 +47,12 @@ function AjaxPostForEquiSubList(url,param){
 				var totalSize = data.length;
 				//根据设备列表，初始化第一台设备的展示信息
 				if(totalSize>0){
-//					var pageSize = 5;
-//					var totalNum = Math.ceil(totalSize/pageSize);
-//					var startIndex = (pageNum-1)*pageSize;
-//					var endIndex = (pageNum*pageSize>totalSize)?totalSize:pageNum*pageSize;
-//					equiId=data[startIndex].equipmentId;
-//					for(let i=startIndex; i<endIndex; i++){
 					equiId=data[0].equipmentId;
 					for(let i=0; i<totalSize; i++){
 						resultList+="<li onclick=\"getEquiData('"+data[i].equipmentId+"');\"><span><em>"+data[i].equipmentName+"</em></span></li>";
 					}
 					$("#equipResultList").html(resultList);
 					getEquiData(equiId);
-//					//加载页码
-//					$("#sub-list-pager").html(loadSubListPages(pageSize,totalSize,pageNum));
-//					$("#sub-list-pager").setSubListPage();
 				}else{
 					$("#equipResultList").html(resultList);
 					getEquiData(null);
--- src/main/resources/static/dist/js/chart/high-chart.js
+++ src/main/resources/static/dist/js/chart/high-chart.js
@@ -2,32 +2,43 @@
  * 绘制highChart图表
  * 此文档用于设置图表的相关配置
  */
+//设置公共配置项【开始】
+var legend={enabled: true, backgroundColor: '#ffffff'};
+var tooltip={shared: true};
+var yAxis={title: {text: '温度值（℃）', style:{color: '#ffffff'} }, labels: {style: {color: '#ffffff'}}, min: 0, max: 50 };
+var plotOptions={spline: {marker: {radius: 1, lineWidth: 1 } } };
+var colors=['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'];
+//设置公共配置项【结束】
+//配置历史数据监测曲线图
 var historyOption = {
 	chart: {zoomType: 'x', backgroundColor: '#21242e' },
 	title: {text: '设备历史温度曲线', style: {color: '#ffffff'}},
-	subtitle: {text: 'laofuzi 2018.12.13', style: {color: '#ffffff'}},
-	legend: {enabled: true },
-	tooltip: {shared: true},
-	yAxis: {title: {text: '温度值（℃）'}, labels: {style: {color: '#ffffff'}} },
-	plotOptions: {spline: {marker: {radius: 1, lineWidth: 1 } } },
-	colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']
+	legend: legend, tooltip: tooltip, yAxis: yAxis, plotOptions: plotOptions, colors: colors
 };
-var historyxAxis={type: 'datetime', labels: {style: {color: '#ffffff'}} };
-//图表1：设备各通道的历史温度曲线
+var history_xAxis={type: 'datetime', labels: {style: {color: '#ffffff'}} };
+//配置当前数据监测统计图
+var currentOption={
+    chart: { type: 'spline', animation: Highcharts.svg, marginRight: 10, backgroundColor: "#21242e", events: {load: addPoints /* 新增系列的坐标值 */ } },
+    time: { useUTC: true },
+    title: { text: '设备通道温度实时监测', style: {color: '#ffffff'} },
+    yAxis: yAxis, tooltip: tooltip, legend: legend, plotOptions: plotOptions, colors: colors,
+};
+var  current_xAxis={type: 'datetime', labels: {style: {color: '#ffffff'}} };
+//图表1：绘制设备各通道的历史温度曲线
 function drawingHistoryChart(url, param){
 	if(url!=null){
 		$.ajax({ url: url, type: "post", data: param, dataType: "json",
 			success: function(result){
-				var data = result.data;
 				var series=[];
+				var data = result.data;
 				var LegendData = data.channelNumArr;
 				var seriesData = data.channelTemArr;
-				historyxAxis.categories=data.receiveTime;
+				history_xAxis.categories=data.receiveTime;
 				for(let i=0; i<LegendData.length; i++){
 					var serie = {name: LegendData[i], data: seriesData[i], type:"spline" };
 					series.push(serie);
 				}
-				historyOption.xAxis=historyxAxis;
+				historyOption.xAxis=history_xAxis;
 				historyOption.series = series;
 				$("#chart_history").highcharts(historyOption);
 			}
@@ -35,61 +46,42 @@ function drawingHistoryChart(url, param){
 	}
 }
 
-//图表2：设备实时监测曲线图
+//图表2：绘制设备实时监测曲线图
+function initCurrentChart(){
+	var startTime = parent.formatInitDateToString(new Date());
+	var endTime = parent.formatDateToString(new Date());
+	var url = "dataAcquisition/historicalCurve";
+	var param = {"equipmentId": equiId, "startTime": startTime, "endTime": endTime };
+	$.ajax({ url: url, type: "post", data: param, dataType: "json",
+		success: function(result){
+			var series=[];
+			var data = result.data;
+			var LegendData = data.channelNumArr;
+			var seriesData = data.channelTemArr;
+			current_xAxis.categories=data.receiveTime;
+			for(let i=0; i<LegendData.length; i++){
+				var serie = {name: LegendData[i], data: seriesData[i], type:"spline" };
+				series.push(serie);
+			}
+			currentOption.xAxis=current_xAxis;
+			currentOption.series = series;
+			$("#chart_current").highcharts(currentOption);
+		}
+	});
+}
+function addPoints() {
+    setInterval(function () {
+    	initCurrentChart();
+    }, 60000);
+}
+//获取当前通道最新温度值
 function getCurrentChartData(url, param){
 	var resultMap={};
 	if(url!=null){
 		$.ajax({ url: url, type: "post", data: param, dataType: "json", async:false,
-			success: function(result){resultMap = result; }
+			success: function(result){resultMap=result;},
+			error: function(){resultMap=null; }
 		});
 	}
 	return resultMap;
-}
-
-var currentChart={
-    chart: {type: 'spline', animation: Highcharts.svg, marginRight: 10, backgroundColor: "#21242e", events: {load: addPoints } },
-    time: { useUTC: false },
-    title: { text: '设备通道温度实时监测', style: {color: '#ffffff'} },
-    xAxis: { type: 'datetime', labels: {style: {color: '#ffffff'}} },
-    yAxis: { title: { text: '温度值（℃）' }, labels: {style: {color: '#ffffff'}} },
-    tooltip: { shared: true },
-    legend: { enabled: true },
-    plotOptions: {spline: {marker: {radius: 1, lineWidth: 1 } } },
-    colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
-    series: [
-    	{name: 'CH1', data: getSeriesData(0) },
-    	{name: 'CH2', data: getSeriesData(1) },
-    	{name: 'CH3', data: getSeriesData(2) },
-    	{name: 'CH4', data: getSeriesData(3) },
-    	{name: 'CH5', data: getSeriesData(4) },
-    	{name: 'CH6', data: getSeriesData(5) }
-    ]
-};
-//随机生成坐标值
-function getSeriesData(num){
-	var data = [], time = (new Date()).getTime(), i;
-	//只展示60个点（一个小时的数据），暂时设定每3秒刷新一次
-	for (i = -59; i <= 0; i += 1) {
-		data.push({ x: time + i * 3000, y: Math.random()*10+num });
-	}
-	return data;
-}
-//新增系列坐标值
-function addPoints() {
-    var series = this.series;
-    setInterval(function () {
-        var x = (new Date()).getTime(), 
-        	y1 = Math.random()*10, 
-        	y2 = Math.random()*10+1,
-        	y3 = Math.random()*10+2,
-        	y4 = Math.random()*10+3, 
-        	y5 = Math.random()*10+4,
-        	y6 = Math.random()*10+5;
-        series[0].addPoint([x, y1], true, true);
-        series[1].addPoint([x, y2], true, true);
-        series[2].addPoint([x, y3], true, true);
-        series[3].addPoint([x, y4], true, true);
-        series[4].addPoint([x, y5], true, true);
-        series[5].addPoint([x, y6], true, true);
-    }, 3000);
 }
